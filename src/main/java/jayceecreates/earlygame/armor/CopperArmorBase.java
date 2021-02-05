27d068d8b397907814c677241205a69b93487625
JayCeeCreates <gagaring.cedric@gmail.com>
2021-02-05 12:06:22 +0800

    Changed armor durability

--- src/main/java/jayceecreates/earlygame/armor/CopperArmorBase.java
+++ src/main/java/jayceecreates/earlygame/armor/CopperArmorBase.java
@@ -19,7 +19,7 @@ public class CopperArmorBase implements ArmorMaterial {
 
     @Override
     public int getDurability(EquipmentSlot slot) {
-        return BASE_DURABILITY[slot.getEntitySlotId()]*12;
+        return BASE_DURABILITY[slot.getEntitySlotId()]*11;
     }
 
     @Override
