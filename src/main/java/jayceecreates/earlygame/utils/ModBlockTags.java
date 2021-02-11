9f20a4abd2cb5efcf5e2a78ac27345836b450494
JayCeeCreates <gagaring.cedric@gmail.com>
2021-02-11 22:49:59 +0800

    1.16.x-1.1.0b : Charm incomp fix (#5)

--- src/main/java/jayceecreates/earlygame/utils/ModBlockTags.java
+++ src/main/java/jayceecreates/earlygame/utils/ModBlockTags.java
@@ -9,6 +9,7 @@ public final class ModBlockTags {
     public static final Tag<Block> ROCK_PLACEABLE_ON = register("rock_placeable_on");
 	public static final Tag<Block> ROCKS = register("rocks");
 	public static final Tag<Block> SLOW_DIGGING = register("slow_digging");
+	public static final Tag<Block> BLACKLISTED_BLOCKS = register("blacklisted_blocks");
 
 	private ModBlockTags() { }
 
