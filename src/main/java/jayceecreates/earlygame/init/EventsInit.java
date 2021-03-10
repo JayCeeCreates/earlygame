474c2bd5819a48a05e2bec81f9b2af9418bae7f1
JayCeeCreates <gagaring.cedric@gmail.com>
2021-03-10 15:21:21 +0800

    Updated to 1.16.x-1.0.0

--- src/main/java/jayceecreates/earlygame/init/EventsInit.java
+++ src/main/java/jayceecreates/earlygame/init/EventsInit.java
@@ -2,6 +2,7 @@ package jayceecreates.earlygame.init;
 
 import jayceecreates.earlygame.utils.DisableWoodStoneTools;
 import jayceecreates.earlygame.utils.FlintKnapEvent;
+import jayceecreates.earlygame.utils.FlintKnifeKnapEvent;
 import jayceecreates.earlygame.utils.RecipeRemover;
 import jayceecreates.earlygame.utils.WoodCuttingEvent;
 
@@ -9,6 +10,7 @@ public class EventsInit {
     public static void init() {
         DisableWoodStoneTools.noStoneWoodTier();
         FlintKnapEvent.knapEvent();
+        FlintKnifeKnapEvent.knifeKnapEvent();
         WoodCuttingEvent.chopEvent();
         RecipeRemover.removeRecipe();
     }
