3184c27c86e6df8671be1f05f9120c4d35d9c801
realguyman <realguyman@protonmail.com>
2022-06-23 14:08:59 -0800

    Optimize imports

--- src/main/java/jayceecreates/earlygame/init/BlocksInit.java
+++ src/main/java/jayceecreates/earlygame/init/BlocksInit.java
@@ -2,10 +2,7 @@ package jayceecreates.earlygame.init;
 
 import jayceecreates.earlygame.EarlyGame;
 import jayceecreates.earlygame.block.*;
-import jayceecreates.earlygame.item.ModItemGroup;
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
