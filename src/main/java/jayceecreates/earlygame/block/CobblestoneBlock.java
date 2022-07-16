52e4c74ced87693b16bf4830a1e0b1b323626d70
JayCeeCreates <gagaring.cedric@gmail.com>
2022-07-16 22:44:09 +0800

    Refactor (#41)

--- src/main/java/jayceecreates/earlygame/block/CobblestoneBlock.java
+++ src/main/java/jayceecreates/earlygame/block/CobblestoneBlock.java
@@ -4,9 +4,9 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 
-public class Cobblestone extends Block {
+public class CobblestoneBlock extends Block {
 
-    public Cobblestone() {
+    public CobblestoneBlock() {
         super(FabricBlockSettings.copy(Blocks.COBBLESTONE));
     }
     
